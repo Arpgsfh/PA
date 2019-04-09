800cb8d799b41e9323b3c1ce01b833dbd7fa5173
DikiIskandar6706160107 <37773684+DikiIskandar6706160107@users.noreply.github.com>
2019-04-10 05:52:23 +0700

    Google Sign-in

--- app/src/main/java/org/d3ifcool/denver/MainActivity.java
+++ app/src/main/java/org/d3ifcool/denver/MainActivity.java
@@ -2,24 +2,53 @@ package org.d3ifcool.denver;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String PROFILE = "profile";
     String idProfil;
     String namaProfile;
-    String reload;
+
+    FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        ImageView signOut = (ImageView) findViewById(R.id.button_sign_out);
         TextView namaProfileTextButton = (TextView) findViewById(R.id.namaAnakTextView);
         Button ubahProfileButton = (Button) findViewById(R.id.ubahProfile);
         Button testButton = (Button) findViewById(R.id.testBtn);
@@ -31,6 +60,28 @@ public class MainActivity extends AppCompatActivity {
         namaProfile = prefs.getString("NAMA", null);
 
         namaProfileTextButton.setText(namaProfile);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if (acct != null) {
+            Uri personPhoto = acct.getPhotoUrl();
+
+            Picasso.with(MainActivity.this)
+                    .load(personPhoto.toString())
+                    .into(signOut);
+        }
+
+
+
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
 
         ubahProfileButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,17 +119,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (idProfil==null){
-            Intent intentProfil = new Intent(MainActivity.this, ProfilAnakActivity.class);
-            startActivity(intentProfil);
-        }
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
     }
 
     @Override
-    public void onRestart() {
-        super.onRestart();
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account==null){
+            Intent intentLogin = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intentLogin);
+            finish();
+
+        }else {
+            if (idProfil==null){
+                Intent intentProfil = new Intent(MainActivity.this, ProfilAnakActivity.class);
+                startActivity(intentProfil);
+                finish();
+            }
+        }
     }
 }
